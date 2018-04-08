ab3a7a4ad14d9dced0669b0e45b2ac7f83f3decb
Lucas Coraça Silva <lucascoracasilva@gmail.com>
2018-04-07 23:58:17 -0300

    test

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,8 +1,22 @@
+// INTRO Animatio
+
+$( document ).ready(function() {
+    $('.the_box').delay(1500).fadeIn(1000);
+    $('.the_box').delay(4000).fadeOut(1000);
+    $('.white_arrow').delay(8000).fadeIn(1000);
+});
+
+
+// NAVBAR Animation
 $(window).on( "scrollstop",  function() {
-    console.log($('.video-foreground').visible( true ));
     if ($('section').visible( true )) {
-      $('nav').animate({ 'backgroundColor': 'rgba(0, 0, 0, 1)' },300);
-    } else {
       $('nav').animate({ 'backgroundColor': 'rgba(0, 0, 0, 0)' },300);
+    } else {
+      $('nav').animate({ 'backgroundColor': 'rgba(0, 0, 0, 1)' },300);
     }
+});
+
+$( ".scroller" ).click(function() {
+    console.log(5 + 6);
+    $("body").animate({ scrollTop: $(window).height()}, 300);
 });
